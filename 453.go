302d6c7803daa25f1f9622208312825bc15304f1
Alfred Xing <alfredxing@users.noreply.github.com>
2017-04-30 16:43:37 -0700

    Small changes & simple docs

--- 453.go
+++ 453.go
@@ -11,6 +11,7 @@ import (
 
 const endpoint string = "https://dns.google.com/resolve"
 
+// An HTTPResponse represents the JSON response object returned by the Google API.
 type HTTPResponse struct {
 	Status     int32          `json:"Status"`
 	TC         bool           `json:"TC"`
@@ -25,11 +26,14 @@ type HTTPResponse struct {
 	Comment    string         `json:"Comment"`
 }
 
+// An HTTPQuestion represents a DNS question part in the JSON response.
 type HTTPQuestion struct {
 	Name string `json:"name"`
 	Type uint16 `json:"type"`
 }
 
+// An HTTPAnswer represents a DNS answer part in the JSON response; it can be
+// part of the answer, authority, or extra sections of the DNS response.
 type HTTPAnswer struct {
 	Name string `json:"name"`
 	Type uint16 `json:"type"`
@@ -39,6 +43,7 @@ type HTTPAnswer struct {
 
 type handler struct{}
 
+// ServeDNS handles DNS queries from clients.
 func (_ handler) ServeDNS(w dns.ResponseWriter, req *dns.Msg) {
 	question := req.Question[0]
 	qType, _ := dns.TypeToString[question.Qtype]
@@ -69,8 +74,9 @@ func (_ handler) ServeDNS(w dns.ResponseWriter, req *dns.Msg) {
 	w.WriteMsg(&msg)
 }
 
+// parse converts an `HTTPResponse` from Google into a `dns.Msg`.
 func parse(res *HTTPResponse, req *dns.Msg) dns.Msg {
-	// Parse response
+	// Parse questions
 	questions := []dns.Question{}
 	for _, q := range res.Question {
 		questions = append(questions, dns.Question{
@@ -83,7 +89,7 @@ func parse(res *HTTPResponse, req *dns.Msg) dns.Msg {
 	// Parse answers
 	answers := []dns.RR{}
 	authorities := []dns.RR{}
-	extras := []dns.RR{}
+	additionals := []dns.RR{}
 
 	for _, a := range res.Answer {
 		answers = append(answers, parseRR(&a))
@@ -92,7 +98,7 @@ func parse(res *HTTPResponse, req *dns.Msg) dns.Msg {
 		authorities = append(authorities, parseRR(&a))
 	}
 	for _, a := range res.Additional {
-		extras = append(extras, parseRR(&a))
+		additionals = append(additionals, parseRR(&a))
 	}
 
 	// Construct response header
@@ -116,12 +122,13 @@ func parse(res *HTTPResponse, req *dns.Msg) dns.Msg {
 		Question: questions,
 		Answer:   answers,
 		Ns:       authorities,
-		Extra:    extras,
+		Extra:    additionals,
 	}
 }
 
+// parseRR converts a single `HTTPAnswer` struct into a single `dns.RR` resource record.
 func parseRR(a *HTTPAnswer) dns.RR {
-	// Construct resource record string
+	// Construct resource record string for parsing
 	typeName, _ := dns.TypeToString[a.Type]
 	resource := fmt.Sprintf("%s %d IN %s %s", a.Name, a.TTL, typeName, a.Data)
 
